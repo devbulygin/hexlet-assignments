4e4256f75eaf7f4f8ca1f39bc589e651ef46229d
@hexlet/cli <support@hexlet.io>
2023-03-13 01:15:58 +0400

    submit java-spring-ru/service-layer

---
+++ java-spring-ru/service-layer/src/main/java/exercise/controller/CityController.java
@@ -0,0 +1,55 @@
+package exercise.controller;
+import exercise.model.City;
+import exercise.repository.CityRepository;
+import exercise.service.WeatherService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RestController
+public class CityController {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Autowired
+    private WeatherService weatherService;
+
+    // BEGIN
+    @GetMapping(path = "/cities/{id}")
+    public Map<String, String> getCity(@PathVariable long id) {
+        return weatherService.lookUp(id);
+    }
+
+    @GetMapping(path = "/search")
+    public List<Map<String, String>> getCities(@RequestParam(required = false) String name) {
+        List<City> filteredCities = new ArrayList<>();
+        if (name == null) {
+            filteredCities = cityRepository.findAllByOrderByName();
+        } else {
+            filteredCities = cityRepository.findByNameStartingWithIgnoreCase(name);
+        }
+
+        return filteredCities.stream()
+                .map(city -> {
+                    Map<String, String> weather = weatherService.lookUp(city.getId());
+                    return Map.of(
+                           "name", city.getName(),
+                           "temperature", weather.get("temperature")
+                    );
+                })
+                .collect(Collectors.toList());
+
+    }
+    // END
+}
+
